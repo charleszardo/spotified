8a830db359218891dbbce9cefcd1430b3c6fae39
Dave Charnuska <davecharnuska@gmail.com>
2017-01-23 14:08:45 -0500

    implement removing songs

--- app/playlists/playlistCtrl.js
+++ app/playlists/playlistCtrl.js
@@ -9,9 +9,14 @@ spotified.controller('PlaylistCtrl', ['playlist', 'songs', function(playlist, so
   self.addSong = function() {
     var trackNo = self.songs.length;
     if (trackNo >= 10) { return; }
-    var song = { data: { title: self.songTitle }, playlistId: self.playlist.$id, ord: trackNo }
+    var song = { data: { title: self.songTitle }, playlistId: self.playlist.$id }
     self.songs.$add(song).then(function (){
       self.songTitle = '';
+    });
+  }
+
+  self.removeSong = function(song) {
+    self.songs.$remove(song).then(function (){
     });
   }
 
