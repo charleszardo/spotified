91dc1bf180fcde978975e1ac465b57eb5d443612
Dave Charnuska <davecharnuska@gmail.com>
2017-01-23 17:54:35 -0500

    implement searching by artist, album, or track

--- app/search/searchCtrl.js
+++ app/search/searchCtrl.js
@@ -3,13 +3,45 @@ spotified.controller('SearchCtrl', ['Spotify', function(Spotify) {
 
   self.searchString = "";
   self.results = [];
+  self.searchType = "track";
+  self.test = "TESTIN!"
 
   self.search = function() {
-    Spotify.search(self.searchString, ['track']).then(function (data) {
+    if (self.searchType === "artist") {
+      self.artistSearch();
+    } else if (self.searchType === "album") {
+      self.albumSearch();
+    } else {
+      self.trackSearch();
+    }
+  }
+
+  self.trackSearch = function() {
+    Spotify.search(self.searchString, 'track').then(function (data) {
       self.results = data.tracks.items;
-      console.log(data);
-      console.log(self.results);
     });
+  }
+
+  self.artistSearch = function() {
+    Spotify.search(self.searchString, 'artist').then(function (data) {
+      angular.forEach(data.artists.items, function(artist) {
+        Spotify.getArtistTopTracks(artist.id, 'US').then(function (data) {
+          self.results = self.results.concat(data.tracks)
+        });
+      });
+    });
+  }
+
+  self.albumSearch = function() {
+    self.results = [];
+    Spotify.search(self.searchString, 'album').then(function (data) {
+      angular.forEach(data.albums.items, function(album) {
+        Spotify.getAlbumTracks(album.id).then(function (data) {
+          console.log(data);
+          self.results = self.results.concat(data.items)
+        });
+      });
+    })
   }
 
 }]);
