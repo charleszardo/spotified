7c724547f7947ba88f40f80b2cfd78450065adb3
Dave Charnuska <davecharnuska@gmail.com>
2017-01-24 00:51:26 -0500

    implement exporting playlist as JSON

--- app/playlists/playlistCtrl.js
+++ app/playlists/playlistCtrl.js
@@ -1,10 +1,34 @@
-spotified.controller('PlaylistCtrl', ['playlist', 'songs', function(playlist, songs) {
+spotified.controller('PlaylistCtrl', ['$window', 'playlist', 'songs', function($window, playlist, songs) {
   var self = this;
 
   self.playlist = playlist;
   self.title = playlist.title;
   self.songs = songs;
   self.songTitle = ""
+
+  self.export = function() {
+    var uri = "data:application/json;charset=UTF-8," + encodeURIComponent(self.playlistJSON());
+
+    $window.open(uri);
+  }
+
+  self.playlistJSON = function() {
+    console.log("OKAY!")
+    var playlistObj = { title: self.title, songs: []},
+        songObj;
+
+    angular.forEach(self.songs, function(song) {
+      songObj = { track: song.data.title,
+                  artist: song.data.artist,
+                  album: song.data.album,
+                  note: song.data.note,
+                  customImage: song.data.customImage };
+
+      playlistObj.songs.push(songObj);
+    })
+
+    return JSON.stringify(playlistObj);
+  }
 
   self.addSong = function(songObj) {
     var trackNo = self.songs.length,
